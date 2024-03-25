cdc76cb3c1d742181139363714b182cd7f91c9af
WindSoilder <WindSoilder@outlook.com>
2024-03-25 11:26:35 +0800

    add setup on conform, use catppuccin theme

--- .config/nvim/sub_init.lua
+++ .config/nvim/sub_init.lua
@@ -756,18 +756,20 @@ require('lazy').setup({
     end,
   },
 
+  -----------------------------------
+  -- Wind Setup(a different theme):
+  -----------------------------------
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin-macchiato'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -1045,6 +1047,35 @@ gitsigns.setup({
 
 -- setup short key for formatting python code
 vim.keymap.set('n', '<leader>f', '<cmd>!black %<CR>', { desc = 'formatting python code use black' })
+
+-- setting up a command to toggle format-on-save
+require("conform").setup({
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
 
 -- setting up nushell language server support.
 local lspconfig = require('lspconfig')
