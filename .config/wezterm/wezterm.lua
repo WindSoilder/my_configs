43e3eba6498d364595ccc49fc7b36ade72d25589
WindSoilder <WindSoilder@outlook.com>
2025-02-07 10:53:24 +0800

    sync wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,18 +1,43 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():toggle_fullscreen()
-    -- window:gui_window():maximize()
-  end)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():toggle_fullscreen()
+	-- window:gui_window():maximize()
+end)
 
 return {
-  font = wezterm.font 'Berkeley Mono Variable',
-  font_size = 19,
-  native_macos_fullscreen_mode = true,
-  color_scheme = "Dracula",
-  hide_tab_bar_if_only_one_tab = true,
-  key = "LeftBracket", mods = "ALT", action = wezterm.action{SendString="\x1b["},
-  key = "RightBracket", mods = "ALT", action = wezterm.action{SendString="\x1b]"}
+	font = wezterm.font("0xProto Nerd Font"),
+	font_size = 14,
+	native_macos_fullscreen_mode = true,
+	-- color_scheme = "Gruvbox dark, hard (base16)",
+	color_scheme = "Nord (base16)",
+	hide_tab_bar_if_only_one_tab = true,
+	front_end = "WebGpu",
+	keys = {
+		{ key = "LeftBracket", mods = "ALT", action = wezterm.action({ SendString = "\x1b[" }) },
+		{ key = "RightBracket", mods = "ALT", action = wezterm.action({ SendString = "\x1b]" }) },
+		{
+			key = "d",
+			mods = "CMD",
+			action = wezterm.action.SplitPane({
+				direction = "Right",
+				size = { Percent = 50 },
+			}),
+		},
+		{
+			key = "D",
+			mods = "CMD",
+			action = wezterm.action.SplitPane({
+				direction = "Down",
+				size = { Percent = 50 },
+			}),
+		},
+		{
+			key = "Enter",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.TogglePaneZoomState,
+		},
+	},
 }
